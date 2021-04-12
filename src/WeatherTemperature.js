ab56679d3a99e9dc3ae4650086a1d6b6d3897803
Marta919 <72815759+Marta919@users.noreply.github.com>
2021-04-12 14:35:50 +0200

    eliminating <a> tag from the code

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -14,15 +14,15 @@ export default function WeatherTemperature (props){
       <div className="temperature">
         <span className="temp"> 
         {Math.round(props.celsius)} </span>
-        <span className="unit">°C | {""}<a href="" onClick={convertToFarenheit} rel="noreferrer">°F</a>  </span>  
-            </div>
+        <span className="unit">°C | {""}<span onClick={convertToFarenheit} rel="noreferrer">°F  </span>  
+           </span> </div>
         );
     }else {
         return (
       <div className="temperature">
         <span className="temp"> 
         {Math.round((props.celsius * 9/5)+32)} </span>
-        <span className="unit"><a href="" onClick={convertToCelsius} rel="noreferrer">°C </a> {""} | °F  </span>  
+        <span className="unit" onClick={convertToCelsius} rel="noreferrer">°C {""} | °F  </span>  
             </div>
         );
 
