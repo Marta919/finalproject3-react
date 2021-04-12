01abe51faaaadf2443de52f0c14e1cbe31df6978
Marta919 <72815759+Marta919@users.noreply.github.com>
2021-04-12 20:11:37 +0200

    took off bootstrap from weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,6 @@
 import React, {useState} from "react";
 import axios from "axios";
 import DetailsWeather from "./DetailsWeather";
-import bootstrap from "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Weather (props) {
   const [weather, setWeather] = useState({loaded : false});
