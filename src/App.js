1d8cf6725f15d125008f25d3fe8a9edc11915b8b
Marta919 <72815759+Marta919@users.noreply.github.com>
2021-04-12 20:23:18 +0200

    adding repository

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ function App() {
    
     <Weather defaultCity= "Roccavivi" />
     </div>
-
+<a href="https://github.com/Marta919" target="_blank" rel="noreferrer">Coded by Marta D'Orazio</a>
+  
 </div>
   );
 }
